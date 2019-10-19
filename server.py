c5ec40e77a9035cda317e6eb5ac05bb1d4a17d0a
Joseph Daniel <josndan@outlook.com>
2019-10-19 13:12:16 -0400

    Update server.py
    Added the meal preference function

--- server.py
+++ server.py
@@ -5,6 +5,10 @@ import mysql.connector
 
 app = Flask(__name__)
 
+prange = 10
+frange = 10
+crange = 10
+
 def db_query(q):   
     c = mysql.connector.connect(
       host="localhost",
@@ -17,12 +21,49 @@ def db_query(q):
 
     query = (q)
     cursor.execute(query)
+    return cursor.fetchall()
 
-    for (r) in cursor:
-        if (float(r[1])>20):
-            print(r)
-        
-db_query("SELECT name, protein FROM nutrition WHERE protein >= 5")
+
+def match1Nutri(ch,value):
+  col=int()
+  res = list()
+  limits = int()
+  if ch =='c' or ch == 'C':
+    col=5
+    res = db_query("SELECT * FROM nutrition")
+    limits = crange
+  elif ch == 'p' or ch == 'P':
+    col=3
+    res = db_query("SELECT * FROM nutrition")
+    limits = prange
+  elif ch == 'f' or ch == 'F':
+    col=4
+    res = db_query("SELECT * FROM nutrition")
+    limits = frange
+  else:
+    raise Exception('Precondition not satisfied')
+
+  for i in range(len(res)-1,-1,-1):
+      if abs(float(res[i][col])-value) > limits: 
+        res.pop(i)
+
+  return set(res)
+
+
+def match(carb,prot,fat,first=3):
+  carbnear = match1Nutri('c',carb)
+  protnear = match1Nutri('p',prot)
+  fatnear = match1Nutri('f',fat)
+  potentData = carbnear.union(protnear,fatnear)
+  potentData = list(potentData)
+  for i in range(len(potentData)):
+    error = (abs(float(potentData[i][3])-prot) + abs(float(potentData[i][4])-fat) + abs(float(potentData[i][5])-carb)) ,
+    mrec = potentData[i] + error
+    potentData[i]= mrec
+  return sorted(potentData,key = lambda x: x[len(x)-1])[:first]
+    
+print(match(12,10,10))        
+
 @app.route("/")
 def home():
     return render_template('index.html')
